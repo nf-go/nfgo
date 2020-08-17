b2b04a6f1f05b6ee581e16c6380f5fd84ff779d3
erich <lzujing@gmail.com>
2020-08-17 11:39:52 +0800

    fix: add error log in rpc error interceptor

--- rpc/interceptor/error_interceptor.go
+++ rpc/interceptor/error_interceptor.go
@@ -6,28 +6,30 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"nfgo.ga/nfgo/nerrors"
+	"nfgo.ga/nfgo/nlog"
 )
 
 // ErrorHandleUnaryServerInterceptor -
 func ErrorHandleUnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	resp, err = handler(ctx, req)
-	err = handleError(err)
+	err = handleError(ctx, err)
 	return
 }
 
 // ErrorHandleStreamServerInterceptor -
 func ErrorHandleStreamServerInterceptor(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 	err := handler(srv, stream)
-	err = handleError(err)
+	err = handleError(stream.Context(), err)
 	return err
 }
 
-func handleError(err error) error {
+func handleError(ctx context.Context, err error) error {
 	if err != nil {
 		// 处理业务错误
 		if bizErr, ok := err.(nerrors.BizError); ok {
 			return grpc.Errorf(codes.Code(bizErr.Code()), bizErr.Msg())
 		}
+		nlog.Logger(ctx).WithError(err).Error()
 		return grpc.Errorf(codes.Internal, nerrors.ErrInternal.Msg())
 	}
 	return nil
